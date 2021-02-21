3da66ed4176df1c9f70b029b0ea8ac41f790d385
meysam <meysam.shohrat@snapp.cab>
2021-02-21 18:20:25 +0330

    add new fragment for handling add phone number to account and login with phone number

---
+++ app/src/main/java/com/ms/playstop/ui/enrerPhoneNumber/EnterPhoneNumberFragment.kt
@@ -0,0 +1,163 @@
+package com.ms.playstop.ui.enrerPhoneNumber
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import com.ms.playstop.R
+import com.ms.playstop.base.BaseFragment
+import com.ms.playstop.extension.*
+import com.ms.playstop.network.model.GeneralResponse
+import com.ms.playstop.ui.login.LoginFragment
+import com.ms.playstop.ui.otp.OtpFragment
+import kotlinx.android.synthetic.main.fragment_enter_phone_number.*
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class EnterPhoneNumberFragment : BaseFragment() {
+
+    companion object {
+        fun newInstance() = EnterPhoneNumberFragment()
+        const val ENTER_PHONE_NUMBER_STATE = "Enter Phone Number State"
+        const val ENTER_PHONE_NUMBER_STATE_ADD = 101
+        const val ENTER_PHONE_NUMBER_STATE_LOGIN = 102
+    }
+
+    private lateinit var viewModel: EnterPhoneNumberViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_enter_phone_number, container, false)
+    }
+
+    override fun tag(): String {
+        return "Enter Phone Number Fragment"
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EnterPhoneNumberViewModel::class.java)
+        handleArguments()
+        initViews()
+        subscribeToViewModel()
+        subscribeToViewEvents()
+    }
+
+    private fun handleArguments() {
+        arguments?.let {
+            if(it.containsKey(ENTER_PHONE_NUMBER_STATE)) {
+                viewModel.state = it.getInt(ENTER_PHONE_NUMBER_STATE, ENTER_PHONE_NUMBER_STATE_LOGIN)
+            }
+        }
+    }
+
+    private fun initViews() {
+        when(viewModel.state) {
+            ENTER_PHONE_NUMBER_STATE_LOGIN -> {
+                enter_phone_number_toolbar_name_tv?.text = getString(R.string.login_with_phone_number)
+                enter_phone_number_btn_divider?.hide()
+                enter_phone_number_skip_btn?.hide()
+            }
+            else -> {
+                enter_phone_number_toolbar_name_tv?.text = getString(R.string.add_phone_number)
+                enter_phone_number_btn_divider?.show()
+                enter_phone_number_skip_btn?.show()
+            }
+        }
+    }
+
+    private fun subscribeToViewModel() {
+        viewModel.login.observe(viewLifecycleOwner, Observer {
+            hideButtonLoading()
+            showToast(GeneralResponse(messageResId = it.second))
+            val otpFragment = OtpFragment.newInstance()
+            otpFragment.arguments = Bundle().apply { 
+                putInt(OtpFragment.VERIFY_STATE,OtpFragment.VERIFY_STATE_LOGIN)
+                putString(OtpFragment.USER_PHONE,it.first)
+            }
+            addToParent(otpFragment)
+        })
+        viewModel.loginError.observe(viewLifecycleOwner, Observer {
+            hideButtonLoading()
+            showToast(it)
+        })
+        
+        viewModel.verify.observe(viewLifecycleOwner, Observer {
+            hideButtonLoading()
+            showToast(GeneralResponse(messageResId = it.second))
+            val otpFragment = OtpFragment.newInstance()
+            otpFragment.arguments = Bundle().apply {
+                putInt(OtpFragment.VERIFY_STATE,OtpFragment.VERIFY_STATE_NONE)
+                putString(OtpFragment.USER_PHONE,it.first)
+            }
+            addToParent(otpFragment)
+        })
+        viewModel.verifyError.observe(viewLifecycleOwner, Observer {
+            hideButtonLoading()
+            showToast(it)
+        })
+    }
+
+    private fun subscribeToViewEvents() {
+        enter_phone_number_btn?.setOnClickListener {
+            showButtonLoading()
+            viewModel.submit(enter_phone_number_phone_et?.text?.toString())
+        }
+        enter_phone_number_back_btn?.setOnClickListener {
+            if(pushBack().not()) {
+                activity?.onBackPressed()
+            }
+        }
+        enter_phone_number_skip_btn?.setOnClickListener {
+            if(pushBack().not()) {
+                activity?.onBackPressed()
+            }
+        }
+        enter_phone_number_root?.setOnClickListener {
+            it.hideSoftKeyboard()
+        }
+    }
+
+    private fun showToast(response: GeneralResponse) {
+        response.message?.takeIf { it.isNotEmpty() }?.let {
+            Toast.makeText(activity,it, Toast.LENGTH_SHORT).show()
+        } ?: kotlin.run {
+            response.messageResId?.let {
+                Toast.makeText(activity,it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun pushBack() : Boolean {
+        return when(viewModel.state) {
+            ENTER_PHONE_NUMBER_STATE_LOGIN -> {
+                replaceInParent(LoginFragment.newInstance())
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    override fun handleBack(): Boolean {
+        return pushBack()
+    }
+
+    private fun showButtonLoading() {
+        enter_phone_number_btn?.text = ""
+        enter_phone_number_btn?.isEnabled = false
+        enter_phone_number_btn_loading?.show()
+    }
+
+    private fun hideButtonLoading() {
+        enter_phone_number_btn_loading?.hide()
+        enter_phone_number_btn?.setText(R.string.submit)
+        enter_phone_number_btn?.isEnabled = true
+    }
+
+}
---
+++ app/src/main/java/com/ms/playstop/ui/enrerPhoneNumber/EnterPhoneNumberViewModel.kt
@@ -0,0 +1,65 @@
+package com.ms.playstop.ui.enrerPhoneNumber
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ms.playstop.R
+import com.ms.playstop.extension.*
+import com.ms.playstop.network.base.ApiServiceGenerator
+import com.ms.playstop.network.model.*
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class EnterPhoneNumberViewModel : ViewModel() {
+
+    val verify : MutableLiveData<Pair<String?,Int>> = MutableLiveData()
+    val verifyError : MutableLiveData<GeneralResponse> = MutableLiveData()
+
+    val login : MutableLiveData<Pair<String?,Int>> = MutableLiveData()
+    val loginError : MutableLiveData<GeneralResponse> = MutableLiveData()
+
+    var state: Int = EnterPhoneNumberFragment.ENTER_PHONE_NUMBER_STATE_LOGIN
+
+    fun submit(phoneNumber: String?) {
+        when(state) {
+            EnterPhoneNumberFragment.ENTER_PHONE_NUMBER_STATE_LOGIN -> {
+                when {
+                    phoneNumber.isNullOrEmpty() -> loginError.value = GeneralResponse(messageResId = R.string.phone_number_can_not_be_empty)
+                    phoneNumber.isValidPhoneNumber().not() -> loginError.value = GeneralResponse(messageResId = R.string.entered_phone_number_is_invalid)
+                    else -> {
+                        ApiServiceGenerator.getApiService
+                            .sendPhoneNumber(PhoneNumberRequest(phoneNumber))
+                            ?.subscribeOn(Schedulers.io())
+                            ?.observeOn(AndroidSchedulers.mainThread())
+                            ?.subscribe({
+                                it?.let {
+                                    login.value = phoneNumber to R.string.otp_sent_successfully
+                                }
+                            },{
+                                loginError.value = GeneralResponse(messageResId = R.string.failed_in_communication_with_server)
+                            })
+                    }
+                }
+            }
+            EnterPhoneNumberFragment.ENTER_PHONE_NUMBER_STATE_ADD -> {
+                when {
+                    phoneNumber.isNullOrEmpty() -> verifyError.value = GeneralResponse(messageResId = R.string.phone_number_can_not_be_empty)
+                    phoneNumber.isValidPhoneNumber().not() -> verifyError.value = GeneralResponse(messageResId = R.string.entered_phone_number_is_invalid)
+                    else -> {
+                        ApiServiceGenerator.getApiService
+                            .addPhoneNumber(PhoneNumberRequest(phoneNumber))
+                            ?.subscribeOn(Schedulers.io())
+                            ?.observeOn(AndroidSchedulers.mainThread())
+                            ?.subscribe({
+                                it?.let {
+                                    verify.value = phoneNumber to R.string.otp_sent_successfully
+                                }
+                            },{
+                                verifyError.value = GeneralResponse(messageResId = R.string.failed_in_communication_with_server)
+                            })
+                    }
+                }
+            }
+            else -> {}
+        }
+    }
+}
