3d440b4b23c0ab30185bb6bbe47aeef47805c1de
meysam <meysam.shohrat@snapp.cab>
2021-02-21 18:01:10 +0330

    add new routes for login with phone, add phone number to account and reset password in ApiService.kt

--- app/src/main/java/com/ms/playstop/network/base/ApiService.kt
+++ app/src/main/java/com/ms/playstop/network/base/ApiService.kt
@@ -7,166 +7,189 @@ import retrofit2.http.*
 
 interface ApiService {
 
-    @GET("config")
+    @GET("v1/config")
     fun getConfig() : Single<ConfigResponse?>?
 
-    @POST("movies/suggestions")
+    @POST("v1/movies/suggestions")
     fun getAllSuggestionsMovies(
         @Body getAllSuggestionsMoviesRequest: GetAllSuggestionsMoviesRequest
     ) : Single<AllSuggestionsMoviesResponse?>?
 
-    @GET("movies/{page}")
+    @GET("v1/movies/{page}")
     fun getLastMovies(
         @Path("page") page : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/{page}/{suggestion_id}")
+    @GET("v1/movies/{page}/{suggestion_id}")
     fun getSuggestionMovies(
         @Path("page") page : Int
         ,@Path("suggestion_id") suggestionId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/special/1/{page}")
+    @GET("v1/movies/special/1/{page}")
     fun getSpecialMovies(
         @Path("page") page : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/director/{id}/{page}")
+    @GET("v1/movies/director/{id}/{page}")
     fun getDirectorMovies(
         @Path("page") page : Int
         ,@Path("id") directorId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/actor/{id}/{page}")
+    @GET("v1/movies/actor/{id}/{page}")
     fun getActorMovies(
         @Path("page") page : Int
         ,@Path("id") actorId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/writer/{id}/{page}")
+    @GET("v1/movies/writer/{id}/{page}")
     fun getWriterMovies(
         @Path("page") page : Int
         ,@Path("id") writerId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/category/{id}/{page}")
+    @GET("v1/movies/category/{id}/{page}")
     fun getCategoryMovies(
         @Path("page") page : Int
         ,@Path("id") categoryId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/quality/{id}/{page}")
+    @GET("v1/movies/quality/{id}/{page}")
     fun getQualityMovies(
         @Path("page") page : Int
         ,@Path("id") qualityId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/language/{id}/{page}")
+    @GET("v1/movies/language/{id}/{page}")
     fun getLanguageMovies(
         @Path("page") page : Int
         ,@Path("id") languageId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/genre/{id}/{page}")
+    @GET("v1/movies/genre/{id}/{page}")
     fun getGenreMovies(
         @Path("page") page : Int
         ,@Path("id") genreId : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movies/year/{year}/{page}")
+    @GET("v1/movies/year/{year}/{page}")
     fun getYearMovies(
         @Path("year") year : Int,
         @Path("page") page : Int
     ) : Single<MoviePagedListResponse?>?
 
-    @GET("movie/{id}")
+    @GET("v1/movie/{id}")
     fun getMovie(
         @Path("id") movieId : Int
     ) : Single<MovieResponse?>?
 
-    @GET("season/{id}")
+    @GET("v1/season/{id}")
     fun getSeason(
         @Path("id") seasonId : Int
     ) : Single<SeasonResponse?>?
 
-    @GET("episode/{id}")
+    @GET("v1/episode/{id}")
     fun getEpisode(
         @Path("id") episodeId : Int
     ) : Single<EpisodeResponse?>?
 
-    @GET("comments/{movie_id}/{page}")
+    @GET("v1/comments/{movie_id}/{page}")
     fun getMovieComments(
         @Path("movie_id") movieId : Int,
         @Path("page") page : Int
     ) : Single<CommentListResponse?>?
 
-    @POST("fb-token")
+    @POST("v1/fb-token")
     fun registerFbToken(
         @Body registerFbTokenRequest: RegisterFbTokenRequest
     ) : Single<GeneralResponse?>?
 
-    @GET("categories")
+    @GET("v1/categories")
     fun getCategories() : Single<CategoryListResponse?>?
 
-    @GET("countries")
+    @GET("v1/countries")
     fun getCountries() : Single<CountryListResponse?>?
 
-    @GET("genres")
+    @GET("v1/genres")
     fun getGenres() : Single<GenreListResponse?>?
 
-    @GET("languages")
+    @GET("v1/languages")
     fun getLanguages() : Single<LanguageListResponse?>?
 
-    @GET("directors/{page}")
+    @GET("v1/directors/{page}")
     fun getDirectors(
         @Path("page") page: Int
     ) : Single<DirectorListResponse?>?
 
-    @GET("writers/{page}")
+    @GET("v1/writers/{page}")
     fun getWriters(
         @Path("page") page: Int
     ) : Single<WriterListResponse?>?
 
-    @GET("actors/{page}")
+    @GET("v1/actors/{page}")
     fun getActors(
         @Path("page") page: Int
     ) : Single<ActorListResponse?>?
 
-    @GET("search/actor")
+    @GET("v1/search/actor")
     fun searchActor(
         @Query("query") query: String?
     ) : Single<ActorListResponse?>?
 
-    @GET("search/director")
+    @GET("v1/search/director")
     fun searchDirector(
         @Query("query") query: String?
     ) : Single<DirectorListResponse?>?
 
-    @GET("search/writer")
+    @GET("v1/search/writer")
     fun searchWriter(
         @Query("query") query: String?
     ) : Single<WriterListResponse?>?
 
-    @GET("search/movie")
+    @GET("v1/search/movie")
     fun searchMovie(
         @Query("query") query: String?
     ) : Single<MovieListResponse?>?
 
-    @POST("comment/{movie_id}")
+    @POST("v1/comment/{movie_id}")
     fun postComment(
         @Body postCommentRequest: PostCommentRequest?,@Path("movie_id") movieId: Int
     ) : Single<GeneralResponse?>?
 
-    @POST("register")
+    @POST("v1/register")
     fun signup(
         @Body signupRequest: SignupRequest?
     ) : Single<GenreListResponse?>?
 
-    @POST("auth/login")
+    @POST("v1/auth/login")
     fun login(
         @Body loginRequest: LoginRequest?
     ) : Single<LoginResponse?>?
 
+    @POST("v1/auth/login/otp")
+    fun sendPhoneNumber(
+        @Body phoneNumberRequest: PhoneNumberRequest?
+    ) : Single<EnterPhoneNumberResponse?>?
+
+    @POST("v1/auth/login/otp/verify")
+    fun loginWithPhoneNumber(
+        @Body loginWithPhoneNumberRequest: LoginWithPhoneNumberRequest?
+    ) : Single<LoginResponse?>?
+
+    @POST("v1/otp/verify")
+    fun verifyPhoneNumber(
+        @Body verifyPhoneNumberRequest: VerifyPhoneNumberRequest?
+    ) : Single<UserResponse?>?
+
+    @POST("v1/otp/request")
+    fun addPhoneNumber(
+        @Body phoneNumberRequest: PhoneNumberRequest?
+    ) : Single<EnterPhoneNumberResponse?>?
+
     @POST()
     fun refreshToken(@Body request: RefreshTokenRequest, @Url url: String = BuildConfig.SERVER_ADDRESS + "oauth/token"): Single<RefreshTokenResponse?>?
+
+    @POST("v1/password/reset")
+    fun resetPassword(@Body resetPasswordRequest: ResetPasswordRequest) : Single<GeneralResponse?>?
 }
