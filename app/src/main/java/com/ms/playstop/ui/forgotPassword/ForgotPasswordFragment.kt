5edb14a3c2fc5bdc9eba6a452c6992ca0bf3340b
meysam <meysam.shohrat@snapp.cab>
2021-07-31 11:29:12 +0430

    handle day night mode change in ForgotPasswordFragment.kt on runtime

--- app/src/main/java/com/ms/playstop/ui/forgotPassword/ForgotPasswordFragment.kt
+++ app/src/main/java/com/ms/playstop/ui/forgotPassword/ForgotPasswordFragment.kt
@@ -1,22 +1,26 @@
 package com.ms.playstop.ui.forgotPassword
 
+import android.content.res.ColorStateList
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 import com.ms.playstop.R
 import com.ms.playstop.base.BaseFragment
-import com.ms.playstop.extension.hide
-import com.ms.playstop.extension.hideSoftKeyboard
-import com.ms.playstop.extension.show
 import com.ms.playstop.network.model.GeneralResponse
 import kotlinx.android.synthetic.main.fragment_forgot_password.*
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_signup.*
 
 class ForgotPasswordFragment : BaseFragment() {
@@ -49,6 +53,37 @@ class ForgotPasswordFragment : BaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
+    }
+
+    override fun onDayNightModeApplied(type: Int) {
+        activity?.let { ctx ->
+            with(ContextCompat.getColor(ctx,R.color.colorPrimary)) {
+                view?.setBackgroundColor(this)
+                forgot_password_btn?.setTextColor(this)
+            }
+            with(ContextCompat.getColor(ctx,R.color.colorAccentDark)) {
+                forgot_password_appbar?.setBackgroundColor(this)
+                forgot_password_email_title_tv?.setTextColor(this)
+            }
+            forgot_password_email_bg_view?.background = MaterialShapeDrawable(
+                ShapeAppearanceModel.builder()
+                .setAllCornerSizes(ctx.resources.getDimensionPixelSize(R.dimen.background_card_radius).toFloat())
+                .build()
+            ).apply {
+                fillColor = ColorStateList.valueOf(ContextCompat.getColor(ctx,R.color.white_opacity_10))
+            }
+            forgot_password_email_et?.setTextColor(ContextCompat.getColor(ctx,R.color.white))
+            forgot_password_email_et?.setHintTextColor(ContextCompat.getColor(ctx,R.color.gray))
+            forgot_password_email_et?.setCompoundDrawablesRelativeWithIntrinsicBounds(AppCompatResources.getDrawable(ctx,R.drawable.ic_email),null,null,null)
+            forgot_password_btn?.background = LayerDrawable(
+                arrayOf(MaterialShapeDrawable(ShapeAppearanceModel.builder()
+                    .setAllCornerSizes(ctx.resources.getDimensionPixelSize(R.dimen.button_radius).toFloat())
+                    .build()), GradientDrawable(
+                    GradientDrawable.Orientation.LEFT_RIGHT,
+                    intArrayOf(ContextCompat.getColor(ctx,R.color.purple),ContextCompat.getColor(ctx,R.color.blue))
+                )
+                ))
+        }
     }
 
     override fun onViewLoaded() {
