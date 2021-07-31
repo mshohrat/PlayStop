b058dc0cb849bd4b16cddb1b555d34ea77867026
meysam <meysam.shohrat@snapp.cab>
2021-07-31 11:38:09 +0430

    handle day night mode change in LinkAdapter.kt on runtime

--- app/src/main/java/com/ms/playstop/ui/movie/adapter/LinkAdapter.kt
+++ app/src/main/java/com/ms/playstop/ui/movie/adapter/LinkAdapter.kt
@@ -2,25 +2,38 @@ package com.ms.playstop.ui.movie.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 import com.google.android.material.textview.MaterialTextView
 import com.ms.playstop.R
+import com.ms.playstop.extension.convertDpToPixel
 import com.ms.playstop.extension.isPlayable
 import com.ms.playstop.model.Subtitle
 import com.ms.playstop.model.Url
 import com.ms.playstop.ui.playVideo.PlayVideoActivity
+import com.ms.playstop.utils.DayNightModeAwareAdapter
+import com.ms.playstop.utils.DayNightModeAwareViewHolder
 import com.ms.playstop.utils.MenuAdapter
 import kotlinx.android.synthetic.main.item_link_layout.view.*
 import java.util.ArrayList
 
-class LinkAdapter(private val urls: List<Url>,private val subtitles: List<Subtitle>? = null,private val movieName: String? = null): RecyclerView.Adapter<LinkAdapter.ViewHolder>() {
+class LinkAdapter(private val urls: List<Url>,private val subtitles: List<Subtitle>? = null,private val movieName: String? = null)
+    : RecyclerView.Adapter<LinkAdapter.ViewHolder>(), DayNightModeAwareAdapter {
+
+    private var recyclerView: RecyclerView? = null
+    private var boundViewHolders = mutableListOf<ViewHolder>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_link_layout,parent,false))
@@ -30,18 +43,35 @@ class LinkAdapter(private val urls: List<Url>,private val subtitles: List<Subtit
          return urls.size
     }
 
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        this.recyclerView = null
+        this.boundViewHolders.clear()
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         if(urls.size > position) {
             val url = urls[position]
             holder.bind(url)
+            boundViewHolders.takeIf { it.contains(holder).not() }?.add(holder)
         }
     }
 
+    override fun onViewRecycled(holder: ViewHolder) {
+        super.onViewRecycled(holder)
+        boundViewHolders.takeIf { it.contains(holder) }?.remove(holder)
+    }
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), DayNightModeAwareViewHolder {
         val rootView = itemView
         val nameTv = itemView.link_name_tv
         val volumeTv = itemView.link_volume_tv
+        val iconIv = itemView.link_icon_iv
         fun bind(url: Url) {
             nameTv?.text = String.format(itemView.context.getString(R.string.quality_x),url.quality?.name ?: "")
             rootView.context?.let { ctx ->
@@ -54,6 +84,27 @@ class LinkAdapter(private val urls: List<Url>,private val subtitles: List<Subtit
                 else {
                     tryToDownload(rootView.context,url)
                 }
+            }
+        }
+
+        override fun onDayNightModeChanged(type: Int) {
+            rootView.context?.let { ctx ->
+                rootView.background = MaterialShapeDrawable(
+                    ShapeAppearanceModel.builder()
+                        .setAllCornerSizes(ctx.resources.getDimensionPixelSize(R.dimen.shimmer_radius).toFloat())
+                        .build()
+                ).apply {
+                    fillColor = ColorStateList.valueOf(ContextCompat.getColor(ctx,R.color.colorPrimaryDark))
+                    strokeColor = ColorStateList.valueOf(ContextCompat.getColor(ctx,R.color.white))
+                    strokeWidth = convertDpToPixel(1f).toFloat()
+                }
+                volumeTv?.setTextColor(ContextCompat.getColor(ctx,R.color.white))
+                nameTv?.setTextColor(ContextCompat.getColor(ctx,R.color.white))
+
+                iconIv?.let {
+                    ImageViewCompat.setImageTintList(it,ColorStateList.valueOf(ContextCompat.getColor(ctx,R.color.colorAccentDark)))
+                }
+
             }
         }
     }
@@ -138,5 +189,11 @@ class LinkAdapter(private val urls: List<Url>,private val subtitles: List<Subtit
                         Intent.createChooser(intent,context.getString(R.string.receive_by))
                     )
          }
+    }
+
+    override fun onDayNightModeChanged(type: Int) {
+        for (item in boundViewHolders) {
+            item.onDayNightModeChanged(type)
+        }
     }
 }
