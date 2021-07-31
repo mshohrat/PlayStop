c9ad1257607c04f31b37295224969c6966c0b194
meysam <meysam.shohrat@snapp.cab>
2021-07-31 11:31:39 +0430

    handle day night mode change in LoginFragment.kt on runtime

--- app/src/main/java/com/ms/playstop/ui/login/LoginFragment.kt
+++ app/src/main/java/com/ms/playstop/ui/login/LoginFragment.kt
@@ -1,5 +1,9 @@
 package com.ms.playstop.ui.login
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,14 +12,17 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 import com.ms.playstop.R
 import com.ms.playstop.base.BaseFragment
 import com.ms.playstop.extension.*
 import com.ms.playstop.network.model.GeneralResponse
 import com.ms.playstop.ui.enrerPhoneNumber.EnterPhoneNumberFragment
 import com.ms.playstop.ui.forgotPassword.ForgotPasswordFragment
-import com.ms.playstop.ui.signup.SignupFragment
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : BaseFragment() {
@@ -49,6 +56,50 @@ class LoginFragment : BaseFragment() {
 
     override fun getEnterAnimation(): Animation? {
         return activity?.let { AnimationUtils.loadAnimation(it,R.anim.fade_in) }
+    }
+
+    override fun onDayNightModeApplied(type: Int) {
+        activity?.let { ctx ->
+            view?.setBackgroundColor(ContextCompat.getColor(ctx,R.color.colorPrimary))
+            with(ContextCompat.getColor(ctx,R.color.colorAccentDark)) {
+                login_appbar?.setBackgroundColor(this)
+                login_email_title_tv?.setTextColor(this)
+                login_password_title_tv?.setTextColor(this)
+            }
+            with(
+                MaterialShapeDrawable(
+                    ShapeAppearanceModel.builder()
+                .setAllCornerSizes(ctx.resources.getDimensionPixelSize(R.dimen.background_card_radius).toFloat())
+                .build()
+            ).apply {
+                fillColor = ColorStateList.valueOf(ContextCompat.getColor(ctx,R.color.white_opacity_10))
+            }) {
+                login_email_bg_view?.background = this
+                login_password_bg_view?.background = this
+            }
+            with(ContextCompat.getColor(ctx,R.color.gray)) {
+                login_email_et?.setHintTextColor(this)
+                login_password_et?.setHintTextColor(this)
+            }
+            with(ContextCompat.getColor(ctx,R.color.white)) {
+                login_password_et?.setTextColor(this)
+                login_forgot_password_btn?.setTextColor(this)
+                login_email_et?.setTextColor(this)
+            }
+            login_btn?.background = LayerDrawable(
+                arrayOf(MaterialShapeDrawable(ShapeAppearanceModel.builder()
+                .setAllCornerSizes(ctx.resources.getDimensionPixelSize(R.dimen.button_radius).toFloat())
+                .build()), GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                    intArrayOf(ContextCompat.getColor(ctx,R.color.purple),ContextCompat.getColor(ctx,R.color.blue))
+                )))
+
+            login_email_et?.setCompoundDrawablesRelativeWithIntrinsicBounds(AppCompatResources.getDrawable(ctx,R.drawable.ic_email),null,null,null)
+            login_password_et?.setCompoundDrawablesRelativeWithIntrinsicBounds(AppCompatResources.getDrawable(ctx,R.drawable.ic_lock),null,null,null)
+            login_btn_divider?.background = GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT,
+                intArrayOf(Color.TRANSPARENT,ContextCompat.getColor(ctx,R.color.gray), Color.TRANSPARENT)
+            )
+        }
     }
 
     override fun onViewLoaded() {
