812097304c8170beb44cef4a3a14890eae9190ce
meysam <meysam.shohrat@snapp.cab>
2021-08-07 17:29:45 +0430

    make RtlLinearLayoutManager.kt open

--- app/src/main/java/com/ms/playstop/utils/RtlLinearLayoutManager.kt
+++ app/src/main/java/com/ms/playstop/utils/RtlLinearLayoutManager.kt
@@ -3,7 +3,7 @@ package com.ms.playstop.utils
 import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
 
-class RtlLinearLayoutManager(context: Context?,orientation: Int,reverseLayout: Boolean): LinearLayoutManager(context,orientation,reverseLayout) {
+open class RtlLinearLayoutManager(context: Context?, orientation: Int, reverseLayout: Boolean): LinearLayoutManager(context,orientation,reverseLayout) {
 
     override fun isLayoutRTL(): Boolean {
         return true
