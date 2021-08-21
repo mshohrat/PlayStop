8bfca756e79ca4aa8293bcbc2c402769585c6c30
meysam <meysam.shohrat@snapp.cab>
2021-08-22 02:08:37 +0430

    add logic for handling movie seen position for users by api calls and within lifecycle events in PlayVideoActivity.kt and PlayVideoViewModel.kt

--- app/src/main/java/com/ms/playstop/ui/playVideo/PlayVideoViewModel.kt
+++ app/src/main/java/com/ms/playstop/ui/playVideo/PlayVideoViewModel.kt
@@ -1,7 +1,48 @@
 package com.ms.playstop.ui.playVideo
 
+import android.annotation.SuppressLint
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ms.playstop.extension.initSchedulers
+import com.ms.playstop.network.base.ApiServiceGenerator
+import com.ms.playstop.network.model.SeenMovieRequest
 
 class PlayVideoViewModel : ViewModel() {
+
+    private var movieId : Int = -1
+    private var currentMovieSeenPosition = 0L
+    val savedMovieSeenPosition = MutableLiveData(0L)
+
+    fun setMovieId(movieId: Int?) {
+        movieId?.let {
+            this.movieId = it
+            fetchSeenMoviePosition()
+        }
+    }
+
+    fun setSeenMoviePosition(position: Long) {
+        this.currentMovieSeenPosition = position
+    }
+
+    @SuppressLint("CheckResult")
+    fun fetchSeenMoviePosition() {
+        ApiServiceGenerator.getApiService
+            .getSeenMovie(movieId)
+            ?.initSchedulers()
+            ?.subscribe({
+               it?.position?.let {
+                   savedMovieSeenPosition.value = it
+               }
+            },{})
+    }
+
+    @SuppressLint("CheckResult")
+    fun updateSeenMovie() {
+        savedMovieSeenPosition.value = currentMovieSeenPosition
+        ApiServiceGenerator.getApiService
+            .updateSeenMovie(movieId,SeenMovieRequest(currentMovieSeenPosition))
+            ?.initSchedulers()
+            ?.subscribe({},{})
+    }
 
 }
