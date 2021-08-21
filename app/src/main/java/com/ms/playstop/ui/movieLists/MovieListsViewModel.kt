2eb8fa4c3485196b587de6e250e006238c121735
meysam <meysam.shohrat@snapp.cab>
2021-08-21 14:59:58 +0430

    update MovieListsViewModel.kt to handle pagination for genresSuggestions movies data on user scroll in movie lists view

--- app/src/main/java/com/ms/playstop/ui/movieLists/MovieListsViewModel.kt
+++ app/src/main/java/com/ms/playstop/ui/movieLists/MovieListsViewModel.kt
@@ -5,16 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ms.playstop.R
 import com.ms.playstop.extension.initSchedulers
-import com.ms.playstop.model.Movie
-import com.ms.playstop.model.SpecialMovies
-import com.ms.playstop.model.Suggestion
-import com.ms.playstop.model.SuggestionMovies
+import com.ms.playstop.model.*
 import com.ms.playstop.network.base.ApiServiceGenerator
 import com.ms.playstop.network.model.ConfigResponse
 import com.ms.playstop.network.model.GeneralResponse
 import com.ms.playstop.network.model.GetAllSuggestionsMoviesRequest
 import com.orhanobut.hawk.Hawk
-import java.util.concurrent.TimeUnit
 
 class MovieListsViewModel : ViewModel() {
 
@@ -26,13 +22,22 @@ class MovieListsViewModel : ViewModel() {
         listOf()
     )
 
+    val moviesExtraList : MutableLiveData<List<GenresSuggestionMovies>> = MutableLiveData(
+        listOf()
+    )
+
+    var isAllExtraMoviesFetched = false
+
     val specialMoviesList : MutableLiveData<ArrayList<Movie>?> = MutableLiveData(
         null
     )
 
+    var moviesPage = 1
+
     init {
         if(Hawk.contains(ConfigResponse.SAVE_KEY)) {
             val config = Hawk.get(ConfigResponse.SAVE_KEY) as? ConfigResponse
+            val l = ArrayList<SuggestionMovies>()
             config?.suggestions?.let {
                 val list = ArrayList<Suggestion>()
                 for (suggestion in it) {
@@ -42,65 +47,94 @@ class MovieListsViewModel : ViewModel() {
                     this.addAll(list)
                 }
                 suggestions.value?.let {
-                    val l = ArrayList<SuggestionMovies>()
                     for (s in it) {
                         l.add(SuggestionMovies(s, listOf()))
                     }
-                    moviesList.value = l
                 }
             }
             config?.specialsName?.let {
                 specialMoviesList.value = arrayListOf()
+                l.add(0,SuggestionMovies(Suggestion(-2, it), listOf()))
             } ?: kotlin.run {
                 specialMoviesList.value = null
             }
+            moviesList.value = l
         }
         fetchMovies()
     }
 
-    fun fetchMovies() {
-        suggestions.value?.let {
-            val suggestionIds = arrayListOf<Int>()
-            for (suggestion in it){
-                suggestionIds.add(suggestion.id)
-            }
-            ApiServiceGenerator.getApiService
-                .getAllSuggestionsMovies(GetAllSuggestionsMoviesRequest(
-                    suggestionIds
-                ))
-                ?.initSchedulers()
-                ?.subscribe({ response ->
-                    var specials : SuggestionMovies? = null
-                    response?.specialMovies?.takeIf { it.movies.size > 5 }?.let {
-                        specials = SuggestionMovies(Suggestion(-2,it.name),it.movies)
-                    }
-                    response?.suggestionMovies?.apply {
-                        specials?.let { s ->
-                            this.add(s)
-                        }
-                    }?.let {
-                        moviesList.value = it
-                    } ?: kotlin.run {
-                        moviesError.value = GeneralResponse(messageResId = R.string.failed_in_communication_with_server)
-                    }
-                    response?.specialMovies?.movies?.takeIf { it.size > 8 }?.let {
-                        specialMoviesList.value = ArrayList(it.take(8))
-                    } ?: kotlin.run {
-                        response?.specialMovies?.movies?.takeIf { it.isNotEmpty() }?.let {
-                            specialMoviesList.value = it
-                        } ?: kotlin.run {
-
-                        }
-                    }
-                },{
-                    moviesError.value = GeneralResponse(messageResId = R.string.failed_in_communication_with_server)
-                })
+    @SuppressLint("CheckResult")
+    fun fetchMovies(fresh: Boolean = false) {
+        if(fresh) {
+            isAllExtraMoviesFetched = false
+            moviesPage = 1
         }
+        if(moviesPage == 1 || fresh) {
+            suggestions.value?.let {
+                val suggestionIds = arrayListOf<Int>()
+                for (suggestion in it) {
+                    suggestionIds.add(suggestion.id)
+                }
+                ApiServiceGenerator.getApiService
+                    .getAllSuggestionsMovies(
+                        GetAllSuggestionsMoviesRequest(
+                            suggestionIds
+                        )
+                    )
+                    ?.initSchedulers()
+                    ?.subscribe({ response ->
+                        var specials: SuggestionMovies? = null
+                        response?.specialMovies?.takeIf { it.movies.size > 5 }?.let {
+                            specials = SuggestionMovies(Suggestion(-2, it.name), it.movies)
+                        }
+                        response?.suggestionMovies?.apply {
+                            specials?.let { s ->
+                                this.add(0,s)
+                            }
+                        }?.let {
+                            moviesList.value = it
+                            moviesPage++
+                        } ?: kotlin.run {
+                            moviesError.value =
+                                GeneralResponse(messageResId = R.string.failed_in_communication_with_server)
+                        }
+                        response?.specialMovies?.movies?.takeIf { it.size > 8 }?.let {
+                            specialMoviesList.value = ArrayList(it.take(8))
+                        } ?: kotlin.run {
+                            response?.specialMovies?.movies?.takeIf { it.isNotEmpty() }?.let {
+                                specialMoviesList.value = it
+                            } ?: kotlin.run {
 
+                            }
+                        }
+                    }, {
+                        moviesError.value =
+                            GeneralResponse(messageResId = R.string.failed_in_communication_with_server)
+                    })
+            }
+        } else {
+            if(isAllExtraMoviesFetched.not()) {
+                ApiServiceGenerator.getApiService
+                    .getAllGenresSuggestionsMovies(moviesPage)
+                    ?.initSchedulers()
+                    ?.subscribe({ response ->
+                        response?.genresSuggestionMovies?.takeIf { it.isNotEmpty() }?.let {
+                            moviesExtraList.value = it
+                            if (response.currentPage < response.totalPages) {
+                                moviesPage++
+                            } else {
+                                isAllExtraMoviesFetched = true
+                            }
+                        }
+                    }, {
+                        isAllExtraMoviesFetched = true
+                    })
+            }
+        }
     }
 
     fun refresh() {
-        fetchMovies()
+        fetchMovies(true)
     }
 
     @SuppressLint("CheckResult")
